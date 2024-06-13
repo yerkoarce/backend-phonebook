624033ba0cdcfabaf7096a5bb676fb05919bfa04
Yerko Arce <yerko.arce.galaz@gmail.com>
2024-06-13 13:00:04 -0400

    exercises 3.1-3.6 completed

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const { v4: uuidv4 } = require('uuid')
+
+const app = express()
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/info', (req, res)=> {
+    const personsQuantity = persons.length
+    const date = Date()
+    res.send(`<p>Phonebook has info for ${personsQuantity} people.</p><p>${date}</p>`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const person = persons.find(p => p.id === id)
+    
+    if(person){
+        res.send(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id',(req, res) =>{
+    const id = req.params.id
+    persons = persons.filter(p => p.id !== id)
+
+    res.status(204).end() 
+})
+
+app.post('/api/persons',(req, res) => {
+    const body = req.body
+    const existingPerson = persons.find(p => p.name === body.name)
+    console.log(existingPerson)
+
+    if (!body.name || !body.number){
+        return res.status(400).json({
+            error: "Name or number missing"
+        })
+    } 
+    if (existingPerson){
+        return res.status(409).json({
+            error: "Phonebook name already registred"
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: uuidv4()
+    }
+
+    persons = persons.concat(person)
+    res.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server listen on port ${PORT}`)
+})
