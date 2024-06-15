063d0f87bbb78c30cdde749291749df3bddb529a
Yerko Arce <yerko.arce.galaz@gmail.com>
2024-06-14 23:31:35 -0400

    actualization of const PORT

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.post('/api/persons',(req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server listen on port ${PORT}`)
 })
