d4193f44b2d550ac5a12abea4d2bc892fa0a296c
Yerko Arce <yerko.arce.galaz@gmail.com>
2024-06-28 13:51:36 -0400

    Se modifica la ruta app.put() para arreglar el proceso de modificación de números

--- index.js
+++ index.js
@@ -59,14 +59,13 @@ app.get('/info', (req, res)=> {
 })
 
 app.put('/api/persons/:id', (req,res,next) => {
-    const body = req.body
+    const { name, number } = req.body
 
-    const person = {
-        name: body.name,
-        number: body.number
-    }
+    Person.findByIdAndUpdate(
+        req.params.id, 
+        { name, number }, 
+        { new: true, runValidators: true, context: 'query' })
 
-    Person.findByIdAndUpdate(req.params.id, person, { new: true })
         .then(updatePerson => {
             res.json(updatePerson)
         })
