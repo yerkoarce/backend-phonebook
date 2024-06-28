357088e772e536a06499585d92e4ab3523bb0972
Yerko Arce <yerko.arce.galaz@gmail.com>
2024-06-28 15:07:52 -0400

    Se mejora el personSchema teniendo requerimientos y validaciones

--- modules/person.js
+++ modules/person.js
@@ -21,7 +21,14 @@ const personSchema = new mongoose.Schema({
     },
     number: {
         type: String,
-        required: true
+        required: true,
+        validate: {
+            validator: function (v) {
+                return /d{2,3}-d{5,8}/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        minLength: 8
     }
 })
 
